df9d5d2bbc3df3d27425d42317c9e12d4bc19c32
jairoy <jairo.yenecura@gmail.com>
2023-11-06 15:01:59 -0300

    Order, Instant, ISO 8601 - PART 2

--- src/main/java/com/jairoy/course/repositories/OrderRepository.java
+++ src/main/java/com/jairoy/course/repositories/OrderRepository.java
@@ -2,8 +2,8 @@ package com.jairoy.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.jairoy.course.entities.User;
+import com.jairoy.course.entities.Order;
 
-public interface OrderRepository extends JpaRepository<User, Long> {	
+public interface OrderRepository extends JpaRepository<Order, Long> {	
 
 }
